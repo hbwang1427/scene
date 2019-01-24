32df58f54f052d4a16edd9729f0e6fb8b192ae0b
memetea <luckykw99@gmail.com>
2019-01-24 10:59:35 +0800

    remove art reference cache

--- model/art.go
+++ model/art.go
@@ -58,14 +58,14 @@ func GetArtReferences() ([]ArtReference, error) {
 
 func loadArtReferences() ([]ArtReference, error) {
 	var references []ArtReference
-	var gobFile = "./artref.gob"
-	if file, err := os.Open(gobFile); err == nil {
-		decoder := gob.NewDecoder(file)
-		decoder.Decode(&references)
-		file.Close()
-		log.Printf("load image references count: %v", len(references))
-		return references, nil
-	}
+	// var gobFile = "./artref.gob"
+	// if file, err := os.Open(gobFile); err == nil {
+	// 	decoder := gob.NewDecoder(file)
+	// 	decoder.Decode(&references)
+	// 	file.Close()
+	// 	log.Printf("load image references count: %v", len(references))
+	// 	return references, nil
+	// }
 
 	rows, err := db.Queryx(sqlGetArtReferences)
 	if err != nil {
